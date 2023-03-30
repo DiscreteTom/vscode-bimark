e969299701c0d2e7ad91d86ca3b92dfe9af28741
DiscreteTom <discrete_tom@outlook.com>
2023-03-30 22:59:39 +0800

    refactor: optimize using bimark v0.3.0

--- src/bimark.ts
+++ src/bimark.ts
@@ -1,28 +1,55 @@
+// https://github.com/microsoft/TypeScript/issues/49721#issuecomment-1319854183
+// @ts-expect-error
+import { EscapedReference, Reference, Definition } from "bimark";
+
+export type DocInfo = {
+  defs: Definition[];
+  refs: Reference[];
+  escaped: EscapedReference[];
+};
+
 export async function init() {
   const { BiMark } = await import("bimark"); // esm import
   const bm = new BiMark();
 
+  /**
+   * `document uri -> { defs, refs, escaped }`
+   */
+  const infoMap = new Map<string, DocInfo>();
+
   return {
     bm,
+    infoMap: infoMap as ReadonlyMap<string, DocInfo>,
     scan: (uri: string, document: string) => {
-      bm.purge(uri);
+      bm.purge(uri); // remove old data
 
+      // ensure docMap has this uri
+      if (!infoMap.has(uri))
+        infoMap.set(uri, { defs: [], refs: [], escaped: [] });
+
+      const doc = infoMap.get(uri)!;
       // re-collect defs
-      bm.collect(uri, document);
+      doc.defs = bm.collectDefs(uri, document);
 
       // debug output
-      const collectedDefs: string[] = [];
-      bm.id2def.forEach((def) => {
-        if (def.path == uri) {
-          collectedDefs.push(def.id);
-        }
-      });
       console.log(
-        `BiMark: collect defs from ${uri}: ${collectedDefs.join(",")}`
+        `BiMark: collect defs from ${uri}: ${doc.defs
+          .map((d) => d.name)
+          .join(",")}`
       );
 
       // re-collect refs
-      bm.render(uri, document);
+      const res = bm.collectRefs(uri, document);
+      doc.refs = res.refs.map((r) => r.ref);
+      doc.escaped = res.escaped.map((r) => r.ref);
+
+      // debug output
+      console.log(`BiMark: collect ${doc.refs.length} refs from ${uri}}`);
+      console.log(
+        `BiMark: collect ${doc.escaped.length} escaped refs from ${uri}`
+      );
+
+      return doc;
     },
   };
 }
